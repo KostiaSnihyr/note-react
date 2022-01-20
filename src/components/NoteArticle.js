1594f4a66b64c756e2a77b2eb4d2cf57cfe82baf
Kostiantyn Snihyr” <kosssm155@gmail.com>
2022-01-20 18:54:31 +0200

    how to start app

--- src/components/NoteArticle.js
+++ src/components/NoteArticle.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const NoteArticle = ({ activeArticleData }) => {
   return (
-    // check if note article not deleted
     <div className="note-article">
       <h1>{activeArticleData.title}</h1>
       <p>{activeArticleData.content}</p>
